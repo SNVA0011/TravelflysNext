a4a9cc08a7fd62fa889b77f7c8e4eb22146a456b
SNVA0011 <96859907+SNVA0011@users.noreply.github.com>
2022-11-23 11:28:35 +0530

    css

--- pages/_document.js
+++ pages/_document.js
@@ -11,8 +11,13 @@ class MyDocument extends Document {
   render() {
     const { lang } = this.props;
     return (
-      <Html lang={lang}>         
+      <Html lang={lang}>
         <Head>
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
 
           {/* <!-- Google Tag Manager --> */}
           <script dangerouslySetInnerHTML={{
