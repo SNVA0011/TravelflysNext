b405c1f79b041e0c611e69e82d35f5184e22e03a
SNVA0011 <96859907+SNVA0011@users.noreply.github.com>
2023-05-24 18:07:32 +0530

    ui

--- pages/ru/blog/[blogDetail].js
+++ pages/ru/blog/[blogDetail].js
@@ -165,7 +165,7 @@ export default function BlogDetails(props, router) {
   }, []);
 
 
-  const decode = (str) => {
+  function decode(str) { 
     let txt = new DOMParser().parseFromString(str, "text/html"); 
     return txt.documentElement.textContent; 
   }
