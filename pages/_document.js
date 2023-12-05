05eb8c7b9b2d74759699c4cc5f9a9292bdcf1925
SNVA0011 <96859907+SNVA0011@users.noreply.github.com>
2023-12-05 11:02:54 +0530

    airlines-pages-ui

--- pages/_document.js
+++ pages/_document.js
@@ -13,12 +13,13 @@ class MyDocument extends Document {
     const { lang } = this.props;
     return (
       <Html lang={lang}>
-        <Head>         
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
+        <Head>
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
 
-          {/* <!-- Google Tag Manager --> */}
-          <script dangerouslySetInnerHTML={{
-            __html: `(function (w, d, s, l, i) {
+      
+            {/* <!-- Google Tag Manager --> */}
+            <script dangerouslySetInnerHTML={{
+              __html: `(function (w, d, s, l, i) {
               w[l] = w[l] || []; w[l].push({
                 'gtm.start':
                   new Date().getTime(), event: 'gtm.js'
@@ -26,7 +27,7 @@ class MyDocument extends Document {
                 j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
                   'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
             })(window, document, 'script', 'dataLayer', 'GTM-MZXPCBQ');`}}></script>
-          {/* <!-- End Google Tag Manager --> */}
+            {/* <!-- End Google Tag Manager --> */}
 
         </Head>
         <body>
