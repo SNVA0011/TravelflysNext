6287acbda9809075eab3339665f45b5dd74cb522
Diptanshu <diptanshu.pathak@snva.com>
2025-02-04 11:26:29 +0530

    DISABLE-COPY

--- pages/_app.js
+++ pages/_app.js
@@ -44,6 +44,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head><meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale = 1.0, user-scalable = no" /></Head>
       <Component {...pageProps} />
+      
     </>
   )
 }
